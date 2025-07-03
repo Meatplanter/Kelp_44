02ac0738b8b83238978ad2c46c8f4a373494f7b9
Meatplanter <noakaron@gmail.com>
2025-07-03 09:04:29 +0200

    Initial commit

---
+++ shoe.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+const distance = 32
+
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("RS_move_left"):
+		velocity.x -= distance
+	if Input.is_action_just_pressed("RS_move_right"):
+		velocity.x += distance
+	if Input.is_action_just_pressed("RS_move_up"):
+		velocity.y -= distance
+	if Input.is_action_just_pressed("RS_move_down"):
+		velocity.y += distance
+	if Input.is_action_just_pressed("RS_move_leftup"):
+		velocity.x -= distance
+		velocity.y -= distance
+	if Input.is_action_just_pressed("RS_move_rightup"):
+		velocity.x += distance
+		velocity.y -= distance
+	if Input.is_action_just_pressed("RS_move_leftdown"):
+		velocity.x -= distance
+		velocity.y += distance
+	if Input.is_action_just_pressed("RS_move_rightdown"):
+		velocity.x += distance
+		velocity.y += distance
+		
+		
+	move_and_slide()
