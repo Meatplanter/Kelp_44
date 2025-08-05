d46b5f36a5bf03685743835c345625b8f2b00266
Meatplanter <72736242+Meatplanter@users.noreply.github.com>
2025-08-05 21:06:27 +0200

    05.08.2025
    - Dzisiaj udało mi się połączyć buty ciałem
    - Rozkminiłem też perpendicular offset (directionals)
    - Przepisałem ciało, żeby buty były zawarte jako dzieci
    - Przygwoździłem kamerę do midpointu między butami
    - Zablokowałem możliwość ruchu zanim but trafi tam gdzie powinien

---
+++ CharBody.gd
@@ -0,0 +1,25 @@
+extends Line2D
+
+var midpoint = Vector2(0,16)
+var focus = Vector2(0,0)
+var bodylength = 32
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	
+func _draw():
+	draw_circle(focus,15.0,Color.RED)
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	set_point_position(0,%LeftShoe.global_position)
+	set_point_position(1,%RightShoe.global_position)
+	midpoint = (get_point_position(1)+get_point_position(0))*0.5
+	var directional = get_point_position(1)-get_point_position(0)
+	var normal = Vector2(-directional.y,directional.x).normalized()
+	focus = midpoint + normal * -128
+	bodylength = get_point_position(0).distance_to(get_point_position(1))
+	#print(bodylength)
+	queue_redraw()
+	pass
--- RightShoe.gd
+++ RightShoe.gd
@@ -3,64 +3,74 @@ extends CharacterBody2D
 var currPos = [32,0]
 var targPos = [32,0]
 var tween: Tween
+var moveTime = 0.5
+var canMove = true
 
 func _input(event):
-	if event.is_action_pressed("RS_move_right"):
-		targPos[0] += 32
-		tween = create_tween()
-		tween.set_parallel(true)
-		tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),0.5).set_trans(Tween.TRANS_BACK)
-		tween.tween_property(self,"rotation_degrees",90,0.5)
+	if canMove == true:
+		if event.is_action_pressed("RS_move_right"):
+			targPos[0] += 32
+			tween = create_tween()
+			tween.set_parallel(true)
+			tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),moveTime).set_trans(Tween.TRANS_BACK)
+			tween.tween_property(self,"rotation_degrees",90,moveTime)
+			
+		elif event.is_action_pressed("RS_move_left"):
+			targPos[0] -= 32
+			tween = create_tween()
+			tween.set_parallel(true)
+			tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),moveTime).set_trans(Tween.TRANS_BACK)
+			tween.tween_property(self,"rotation_degrees",270,moveTime)
+			
+		elif event.is_action_pressed("RS_move_up"):
+			targPos[1] -= 32
+			tween = create_tween()
+			tween.set_parallel(true)
+			tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),moveTime).set_trans(Tween.TRANS_BACK)
+			tween.tween_property(self,"rotation_degrees",0,moveTime)
+			
+		elif event.is_action_pressed("RS_move_down"):
+			targPos[1] += 32
+			tween = create_tween()
+			tween.set_parallel(true)
+			tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),moveTime).set_trans(Tween.TRANS_BACK)
+			tween.tween_property(self,"rotation_degrees",180,moveTime)
+			
+		elif event.is_action_pressed("RS_move_rightup"):
+			targPos[0] += 32
+			targPos[1] -= 32
+			tween = create_tween()
+			tween.set_parallel(true)
+			tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),moveTime).set_trans(Tween.TRANS_BACK)
+			tween.tween_property(self,"rotation_degrees",45,moveTime)
+			
+		elif event.is_action_pressed("RS_move_rightdown"):
+			targPos[0] += 32
+			targPos[1] += 32
+			tween = create_tween()
+			tween.set_parallel(true)
+			tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),moveTime).set_trans(Tween.TRANS_BACK)
+			tween.tween_property(self,"rotation_degrees",135,moveTime)
+			
+		elif event.is_action_pressed("RS_move_leftup"):
+			targPos[0] -= 32
+			targPos[1] -= 32
+			tween = create_tween()
+			tween.set_parallel(true)
+			tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),moveTime).set_trans(Tween.TRANS_BACK)
+			tween.tween_property(self,"rotation_degrees",305,moveTime)
+			
+		elif event.is_action_pressed("RS_move_leftdown"):
+			targPos[0] -= 32
+			targPos[1] += 32
+			tween = create_tween()
+			tween.set_parallel(true)
+			tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),moveTime).set_trans(Tween.TRANS_BACK)
+			tween.tween_property(self,"rotation_degrees",225,moveTime)
 		
-	elif event.is_action_pressed("RS_move_left"):
-		targPos[0] -= 32
-		tween = create_tween()
-		tween.set_parallel(true)
-		tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),0.5).set_trans(Tween.TRANS_BACK)
-		tween.tween_property(self,"rotation_degrees",270,0.5)
-		
-	elif event.is_action_pressed("RS_move_up"):
-		targPos[1] -= 32
-		tween = create_tween()
-		tween.set_parallel(true)
-		tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),0.5).set_trans(Tween.TRANS_BACK)
-		tween.tween_property(self,"rotation_degrees",0,0.5)
-		
-	elif event.is_action_pressed("RS_move_down"):
-		targPos[1] += 32
-		tween = create_tween()
-		tween.set_parallel(true)
-		tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),0.5).set_trans(Tween.TRANS_BACK)
-		tween.tween_property(self,"rotation_degrees",180,0.5)
-		
-	elif event.is_action_pressed("RS_move_rightup"):
-		targPos[0] += 32
-		targPos[1] -= 32
-		tween = create_tween()
-		tween.set_parallel(true)
-		tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),0.5).set_trans(Tween.TRANS_BACK)
-		tween.tween_property(self,"rotation_degrees",45,0.5)
-		
-	elif event.is_action_pressed("RS_move_rightdown"):
-		targPos[0] += 32
-		targPos[1] += 32
-		tween = create_tween()
-		tween.set_parallel(true)
-		tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),0.5).set_trans(Tween.TRANS_BACK)
-		tween.tween_property(self,"rotation_degrees",135,0.5)
-		
-	elif event.is_action_pressed("RS_move_leftup"):
-		targPos[0] -= 32
-		targPos[1] -= 32
-		tween = create_tween()
-		tween.set_parallel(true)
-		tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),0.5).set_trans(Tween.TRANS_BACK)
-		tween.tween_property(self,"rotation_degrees",305,0.5)
-		
-	elif event.is_action_pressed("RS_move_leftdown"):
-		targPos[0] -= 32
-		targPos[1] += 32
-		tween = create_tween()
-		tween.set_parallel(true)
-		tween.tween_property(self,"position",Vector2(targPos[0],targPos[1]),0.5).set_trans(Tween.TRANS_BACK)
-		tween.tween_property(self,"rotation_degrees",225,0.5)
+func _process(delta: float) -> void:
+	currPos = [int(global_position.x),int(global_position.y)]
+	if currPos == targPos:
+		canMove = true
+	else:
+		canMove = false
