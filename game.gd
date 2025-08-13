69eb1321220a2379c2f51e7283169b4156dc915d
Meatplanter <72736242+Meatplanter@users.noreply.github.com>
2025-08-13 14:33:23 +0200

    Game modes added (barebone)

--- game.gd
+++ game.gd
@@ -13,6 +13,12 @@ func spawn_bullet():
 	new_bullet.look_at(Global.CharBodyNode.midpoint)
 	add_child(new_bullet)
 
+func spawn_enemy():
+	var new_enemy = preload("res://enemy.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	new_enemy.global_position = %PathFollow2D.global_position
+	add_child(new_enemy)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -30,8 +36,23 @@ func _process(delta: float) -> void:
 		$BulletSpawnTime.set_paused(0)
 	Global.gameSpeed = gameSpeed
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ChangeMode") && Global.gameMode == 0:
+		Global.gameMode = 1
+		print("Mode is now: Spawn enemies")
+	elif event.is_action_pressed("ChangeMode") && Global.gameMode == 1:
+		Global.gameMode = 0
+		print("Mode is now: Spawn bullets")
+		
+
 func _on_bullet_spawn_time_timeout() -> void:
-	spawn_bullet()
+	if Global.gameMode == 0:
+		spawn_bullet()
+	elif Global.gameMode == 1:
+		spawn_enemy()
 
 func _on_slomo_bullet_spawn_time_timeout() -> void:
-	spawn_bullet()
+	if Global.gameMode == 0:
+		spawn_bullet()
+	elif Global.gameMode == 1:
+		spawn_enemy()
--- global.gd
+++ global.gd
@@ -5,4 +5,6 @@ var RightShoeNode = null
 var LeftShoeNode = null
 
 var gameSpeed = 1.0 
+var gameMode = 0 # 0 = spawn bullets 1 = spawn enemies
+
 var midpoint = Vector2(0,16)
