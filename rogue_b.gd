efcbf9e193710e484398578ec0e2053bc23945a3
Meatplanter <72736242+Meatplanter@users.noreply.github.com>
2025-09-21 19:07:01 +0200

    Movement revamp cont.

--- rogue_b.gd
+++ rogue_b.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 var moveCooldown = false
 
+func other_shoe(body: Node2D):
+	if body == %LeftShoe: return %RightShoe
+	elif body == %RightShoe: return %LeftShoe
+
 func check_collision(point: Vector2) -> bool:
 	var space_state = get_world_2d().direct_space_state
 	var params = PhysicsPointQueryParameters2D.new()
@@ -13,11 +17,10 @@ func check_collision(point: Vector2) -> bool:
 	if result.size() > 0: return true
 	else: return false
 
-func can_move(body: Node2D, dir: Vector2) -> bool: #check if target movement point has collisions or is beyond max movement distance
+#check if target movement point has collisions or is beyond max movement distance or on cooldown
+func can_move(body: Node2D, dir: Vector2) -> bool:
 	
-	var other = Node2D
-	if body == %LeftShoe: other = %RightShoe
-	elif body == %RightShoe: other = %LeftShoe
+	var other = other_shoe(body)
 	
 	var targetPos = body.global_position + dir * Movement.moveDistance
 	var dist = targetPos.distance_to(other.global_position)
@@ -33,44 +36,61 @@ func is_weighted(body: Node2D):
 	elif body == %RightShoe and Movement.rightWeighted == true: return true
 	elif body == %RightShoe and Movement.rightWeighted == false: return false
 
-func shift_weight():
-	
-	if Movement.leftWeighted == true:
-		Movement.leftWeighted = false
-		Movement.rightWeighted = true
-	elif Movement.rightWeighted == true:
-		Movement.leftWeighted = true
-		Movement.rightWeighted = false
+#func shift_weight():
+	##if Movement.leftWeighted == true and Movement.rightWeighted == true:
+	#if Movement.leftWeighted == true:
+		#Movement.leftWeighted = false
+		#Movement.rightWeighted = true
+	#elif Movement.rightWeighted == true:
+		#Movement.leftWeighted = true
+		#Movement.rightWeighted = false
 	
 
-func move_time(body: Node2D, dir: Vector2):
-		
-	var other = Node2D
-	if body == %LeftShoe: other = %RightShoe
-	elif body == %RightShoe: other = %LeftShoe
+func move_type(body: Node2D, dir: Vector2):
+	
+	var other = other_shoe(body)
 	
 	var targetPos = body.global_position + dir * Movement.moveDistance
 	var dist = targetPos.distance_to(other.global_position)
+	
+	if is_weighted(body) == false && dist == Movement.moveDistance: return 1 #Movement.moveSimpleOptimal
+	elif is_weighted(body) == true && dist == Movement.moveDistance: return 2 #Movement.moveSimpleHeavy
+	elif is_weighted(body) == false && dist > Movement.moveDistance: return 3 #Movement.moveComplexOptimal
+	elif is_weighted(body) == true && dist > Movement.moveDistance: return 4 #Movement.moveComplexHeavy
+
+func move_time(body: Node2D, dir: Vector2):
 	
 	var diagonal
 	if dir == Movement.DirRight or dir == Movement.DirLeft or dir == Movement.DirUp or dir == Movement.DirDown: diagonal = 1
 	else: diagonal = sqrt(2)
 	
-	if is_weighted(body) == false && dist == Movement.moveDistance: return Movement.moveSimpleOptimal * diagonal
-	elif is_weighted(body) == true && dist == Movement.moveDistance: return Movement.moveSimpleHeavy * diagonal
-	elif is_weighted(body) == false && dist > Movement.moveDistance: return Movement.moveComplexOptimal * diagonal
-	elif is_weighted(body) == true && dist > Movement.moveDistance: return Movement.moveComplexHeavy * diagonal
+	if move_type(body, dir) == 1: return Movement.moveSimpleOptimal * diagonal
+	elif move_type(body, dir) == 2: return Movement.moveSimpleHeavy * diagonal
+	elif move_type(body, dir) == 3: return Movement.moveComplexOptimal * diagonal
+	elif move_type(body, dir) == 4: return Movement.moveComplexHeavy * diagonal
+
+func shift_weight(body:Node2D,dir:Vector2):
+	var moveType = move_type(body,dir)
+	var moveTime = move_time(body,dir)
+	#Ok, pomysł: poruszanie się ma od 0 do 2 przenosin ciężaru. 
+	#Dzielimy czas ruchu na takie segmenty i wtedy w zależności od momentu zmieniamy ciężar 
 
 func move(body:Node2D, dir: Vector2):
 	if can_move(body,dir) == true:
+		var moveType = move_type(body,dir)
 		var moveTime = move_time(body,dir)
+		
 		moveCooldown = true
 		$MovementCooldown.wait_time = moveTime
 		$MovementCooldown.start()
+		
 		var dist = Movement.moveDistance
+		
 		var tween: Tween
 		tween = create_tween()
 		tween.tween_property(body,"position",body.global_position + dir * dist,moveTime).set_trans(Movement.styleTween)
+
+
 
 #func _ready():
 	#if can_move($RightShoe,Movement.DirRight) == true: print("yay")
